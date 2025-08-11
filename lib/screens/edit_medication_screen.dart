e20dea1e492ee86fe14a17d922c4a4ff1dc72e25
giyunlee <dlrldbs6@hotmail.com>
2025-08-11 18:16:56 +0900

    Fix: Remove circular import in edit_medication_screen.dart

--- lib/screens/edit_medication_screen.dart
+++ lib/screens/edit_medication_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:pilltongapp/models/medication.dart';
-import 'package:pilltongapp/screens/edit_medication_screen.dart';
 
 class EditMedicationScreen extends StatefulWidget {
   final Medication medication;
